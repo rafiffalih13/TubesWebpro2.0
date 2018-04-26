508ac69f61f49a7a8bfe6c96dfb792453bfb17db
Rafif Falih <rafiffalih13@gmail.com>
2018-04-26 22:44:46 +0700

    akhirnya bisa

--- application/controllers/c_home.php
+++ application/controllers/c_home.php
@@ -1,7 +1,7 @@
 <?php 
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class home extends CI_Controller {
+	class c_home extends CI_Controller {
 		
 		function __construct(){
 			parent::__construct();
