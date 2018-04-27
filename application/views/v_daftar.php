b889db4adebb71f9218303731c7b21cc084cc38a
Rafif Falih <rafiffalih13@gmail.com>
2018-04-28 00:34:25 +0700

    stress input

--- application/views/v_daftar.php
+++ application/views/v_daftar.php
@@ -14,6 +14,7 @@
 <!--===============================================================================================-->
 </head>
 <body>	
+	<?php echo form_open('c_daftar/simpan');?>
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
