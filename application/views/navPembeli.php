6e97061ae53442e379837ba6d9c3f796e4f73ef3
AZzuma <meinzuma@gmail.com>
2018-05-01 17:14:03 +0700

    tambah nav

---
+++ application/views/navPembeli.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    
+
+    <!-- CSS NAVBAR -->
+    <link rel="stylesheet" type="text/css" href="css/navbar.css" />
+    
+    <!-- ICON -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    
+    <nav class="navbar text-white fixed-top navbar-expand-lg navbar-info bg-info fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="" width="auto" height="35"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="coba fa fa-reorder"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav mx-auto">
+            <li class="nav-item">
+              <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2 cari" type="search" placeholder="Cari produk" aria-label="Search">
+                  <button class="btn btn-info my-2 my-sm-0" type="submit"><i class="fa fa-search"></i><span>  </span>Cari</button>
+                </form>
+          </li>
+          </ul>
+          <ul class="navbar-nav ml-auto samping">
+            
+            <li class="nav-item dropdown kiri">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 Akun
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
+                <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
+                <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
+                <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
+              </div>
+            </li>
+            <li class="nav-item" style="margin-left: 15px"><a href="#band" style="word-spacing: 3px" class="nav-link"><span> </span><span>  </span><span>  </span></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
