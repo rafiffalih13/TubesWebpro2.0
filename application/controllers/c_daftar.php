01d3551c63b5303171c7849a0af336970b4da8a2
Rafif Falih <rafiffalih13@gmail.com>
2018-05-01 14:52:49 +0700

    saya stress

--- application/controllers/c_daftar.php
+++ application/controllers/c_daftar.php
@@ -11,7 +11,7 @@
 		}
 
 		public function index(){
-			$this->load->view("v_daftar");
+			$this->load->view("daftar");
 		}
 
 		function simpan(){
---
+++ application/views/daftar.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Daftar Akun</title>
+	<!-- Icon Title -->
+	<link rel="icon" type="image/png" href="<?php echo base_url();?>assets/img/logo1.png">
+	<!-- bootstrap css -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<!-- daftar.html css -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/daftar.css">
+	<!-- bootstrap js -->
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+	<!-- jquery -->
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.1.1.js"></script>
+	<!-- plugin validasi form -->
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.validate.js"></script>
+	<!-- rules & massage validasi form -->
+	<script src="<?php echo base_url();?>assets/js/form-validasi.js"></script>
+	<!-- javascript agar form nomor telfon hanya bisa numeric -->
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/daftar.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col"></div>
+			<!-- Start isi Konten -->
+			<div class="col-4 center">
+				<a href="index.html">
+					<img class="logo" src="<?php echo base_url();?>assets/img/logo1.png">
+				</a>
+				<div class="daftar">
+					Daftar Akun Sekarang
+				</div>
+				<div class="disini">
+					<p>Sudah punya akun? klik <a href="login.html" style="color: ">disini</a></p>
+				</div>
+				<!-- Form Daftar Start -->
+				<?php echo form_open('c_daftar/simpan');?>
+				<form id="FormDaftar" action="<?php echo base_url(). 'index.php/c_daftar/simpan';?>">
+					<div class="form-group">
+						<div class="col-sm-15">
+							<input class="form-control" type="text" name="username" id="username" placeholder="Enter username">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-19">
+							<input class="form-control" type="password" name="pass" id="pass" placeholder="Enter password">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-19">
+							<input class="form-control" type="password" name="cpass" id="cpass" placeholder="Confirm password">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-19">
+							<input class="form-control" type="text" name="nama" id="nama" placeholder="Enter Full Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-19">
+							<input class="form-control" type="text" name="email" id="email" placeholder="Enter Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-19">
+							<input class="form-control" type="text" name="no_hp" id="no_hp" placeholder="Enter Phone Number">
+						</div>
+					</div>
+					<div class="form-group left">
+						<div class="col-sm-19">
+							<input class="form-control" type="text" name="alamat" id="alamat" placeholder="Enter Address">
+						</div>
+					</div>
+					<div class="form-group left">
+						<div class="col-sm-0">
+							<p>Dengan klik daftar, kamu telah menyetujui Aturan Penggunaan dan Kebijakan Privasi dari Big Shop</p>
+							<button type="submit" class="btn tmbl">DAFTAR</button>
+						</div>
+					</div>
+				</form>
+				<!-- Form Daftar End -->
+			</div>
+			<!-- Center Div end -->
+			<div class="col"></div>
+		</div>			
+	</div>
+</body>
+</html>
