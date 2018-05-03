d05def76c365440475d126b43d7647b85f34f479
irfannnnn <irfan.prakoso.idp@gmail.com>
2018-05-04 00:19:30 +0700

    edit query m_home

--- application/models/m_home.php
+++ application/models/m_home.php
@@ -7,12 +7,13 @@
                 //         parent::__construct();
                 // }
                 public function get_data_barang() {
-               		return $this->db->get('barang')->result();
+               		// return $this->db->get('barang')->result();
 
-     //           		$this->db->select('*');
-					// $this->db->from('barang');
-					// $this->db->join('akun_penjual', 'barang.username = akun_penjual.username');
-					// return $this->db->get()->result();
+                	$this->db->select('barang.id_barang, barang.username, barang.nama_barang, barang.images, barang.harga, barang.kategori, akun_penjual.nama_toko');
+					$this->db->from('barang');
+					$this->db->join('akun_penjual', 'barang.username = akun_penjual.username');
+					$query = $this->db->get();
+					return $query->result();
                 }
 
         }
